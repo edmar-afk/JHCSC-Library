fcf472ec3c399e89214d71512f2aa8b32416c8fa
project2479 <jaywrsnp6@gmail.com>
2024-04-16 14:29:58 +0800

    change color

--- src/components/NavigateUp.jsx
+++ src/components/NavigateUp.jsx
@@ -28,7 +28,7 @@ function NavigateUp() {
 				onClick={handleScrollToTop}
 				className={`${
 					scrollPosition > 250 ? "-translate-x-px" : "translate-x-24 opacity-0 duration-[2000ms]"
-				} right-6 bottom-6 fixed z-50 p-3 px-4 text-white hover:bg-slate-950 hover:text-yellow-300 hover:scale-110 cursor-pointer duration-500 rounded-lg bg-yellow-300`}>
+				} right-6 bottom-6 fixed z-50 p-3 px-4 text-white hover:bg-slate-950 hover:text-green-300 hover:scale-110 cursor-pointer duration-500 rounded-lg bg-green-950`}>
 				<FontAwesomeIcon
 					icon={faArrowAltCircleUp}
 					className="animate-bounce"
