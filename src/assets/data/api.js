b8f78d797c2365476594c061a55f67f113e6bc6c
project2479 <jaywrsnp6@gmail.com>
2024-04-17 10:46:10 +0800

    new schedule

--- src/assets/data/api.js
+++ src/assets/data/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://libraryapi.pythonanywhere.com/";
+const API_URL = "http://127.0.0.1:8000/";
 export default API_URL;
 
 // ("https://libraryapi.pythonanywhere.com/");
--- src/components/pagesComponents/homepage/Example.jsx
+++ src/components/pagesComponents/homepage/Example.jsx
@@ -1,4 +1,13 @@
-/* eslint-disable react/prop-types */ import {	faArrowAltCircleLeft,	faArrowAltCircleRight,} from "@fortawesome/free-solid-svg-icons";import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";import {	add,	eachDayOfInterval,	endOfMonth,	format,
+/* eslint-disable react/prop-types */ import {	faArrowAltCircleLeft,	faArrowAltCircleRight,
+	faBuildingLock,
+	faXmarkCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	add,
+	eachDayOfInterval,
+	endOfMonth,
+	format,
 	getDay,
 	isEqual,
 	isSameDay,
@@ -8,51 +17,36 @@
 	parseISO,
 	startOfToday,
 } from "date-fns";
-import { useState } from "react";
-
-const meetings = [
-	{
-		id: 1,
-		name: "Capstone defend for BSIT",
-		imageUrl: "https://t3.ftcdn.net/jpg/01/38/48/40/360_F_138484065_1enzXuW8NlkppNxSv4hVUrYoeF8qgoeY.jpg",
-		startDatetime: "2024-04-11T13:00",
-		endDatetime: "2024-05-11T14:30",
-	},
-	{
-		id: 2,
-		name: "",
-		imageUrl: "https://t3.ftcdn.net/jpg/01/38/48/40/360_F_138484065_1enzXuW8NlkppNxSv4hVUrYoeF8qgoeY.jpg",
-		startDatetime: "2024-05-20T09:00",
-		endDatetime: "2024-05-20T11:30",
-	},
-	{
-		id: 3,
-		name: "",
-		imageUrl: "https://t3.ftcdn.net/jpg/01/38/48/40/360_F_138484065_1enzXuW8NlkppNxSv4hVUrYoeF8qgoeY.jpg",
-		startDatetime: "2024-05-20T17:00",
-		endDatetime: "2024-05-20T18:30",
-	},
-	{
-		id: 4,
-		name: "",
-		imageUrl: "https://t3.ftcdn.net/jpg/01/38/48/40/360_F_138484065_1enzXuW8NlkppNxSv4hVUrYoeF8qgoeY.jpg",
-		startDatetime: "2024-06-09T13:00",
-		endDatetime: "2024-06-09T14:30",
-	},
-	{
-		id: 5,
-		name: "",
-		imageUrl: "https://t3.ftcdn.net/jpg/01/38/48/40/360_F_138484065_1enzXuW8NlkppNxSv4hVUrYoeF8qgoeY.jpg",
-		startDatetime: "2024-05-13T14:00",
-		endDatetime: "2024-05-13T14:30",
-	},
-];
+import logo from "../../../assets/img/logo.png";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import API_URL from "../../../assets/data/api";
+// State to store the bookings data
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(" ");
 }
 
 export default function Example() {
+	const [bookings, setBookings] = useState([]);
+
+	useEffect(() => {
+		// Function to fetch data
+		const fetchBookings = async () => {
+			try {
+				// Make GET request to the API endpoint
+				const response = await axios.get(`${API_URL}api/booking/`);
+				// Set the bookings state with the data from the response
+				setBookings(response.data);
+			} catch (error) {
+				console.error("Error fetching bookings:", error);
+			}
+		};
+
+		// Call the fetchBookings function
+		fetchBookings();
+	}, []);
+
 	let today = startOfToday();
 	let [selectedDay, setSelectedDay] = useState(today);
 	let [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"));
@@ -73,11 +67,11 @@ export default function Example() {
 		setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
 	}
 
-	let selectedDayMeetings = meetings.filter((meeting) => isSameDay(parseISO(meeting.startDatetime), selectedDay));
+	let selectedDayMeetings = bookings.filter((meeting) => isSameDay(parseISO(meeting.startDatetime), selectedDay));
 
 	return (
-		<div className="pt-16">
-			<div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
+		<div className="pt-2">
+			<div className="max-w-md px-4 mx-auto md:max-w-6xl">
 				<div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
 					<div className="md:pr-14">
 						<div className="flex items-center">
@@ -106,11 +100,11 @@ export default function Example() {
 							<div>F</div>
 							<div>S</div>
 						</div>
-						<div className="grid grid-cols-7 mt-2 text-sm">
+						<div className="grid grid-cols-7 mt-0 text-sm">
 							{days.map((day, dayIdx) => (
 								<div
 									key={day.toString()}
-									className={classNames(dayIdx === 0 && colStartClasses[getDay(day)], "py-1.5")}>
+									className={classNames(dayIdx === 0 && colStartClasses[getDay(day)], "py-3")}>
 									<button
 										type="button"
 										onClick={() => setSelectedDay(day)}
@@ -134,16 +128,21 @@ export default function Example() {
 										<time dateTime={format(day, "yyyy-MM-dd")}>{format(day, "d")}</time>
 									</button>
 
-									<div className="w-1 h-1 mx-auto mt-1">
-										{meetings.some((meeting) => isSameDay(parseISO(meeting.startDatetime), day)) && (
-											<div className="w-1 h-1 rounded-full bg-sky-500"></div>
+									<div className="mx-auto -mt-0.5 relative">
+										{bookings.some((meeting) => isSameDay(parseISO(meeting.startDatetime), day)) && (
+											<div className="text-center">
+												<FontAwesomeIcon
+													icon={faXmarkCircle}
+													className="text-red-600 animate-bounce"
+												/>
+											</div>
 										)}
 									</div>
 								</div>
 							))}
 						</div>
 					</div>
-					<section className="mt-12 md:mt-0 md:pl-14">
+					<section className="mt-12 md:mt-0 md:pl-4">
 						<h2 className="font-semibold text-gray-900">
 							Schedule for{" "}
 							<time dateTime={format(selectedDay, "yyyy-MM-dd")}>{format(selectedDay, "MMM dd, yyy")}</time>
@@ -164,7 +163,9 @@ export default function Example() {
 											alt=""
 											className="flex-none w-1/2"
 										/>
-										<p className="text-bold text-xl mt-12">No booking for today.</p>
+										<p className="text-bold text-xl mt-12 text-gray-800">
+											No upcoming booking for {format(selectedDay, "MMM dd")}.
+										</p>
 									</div>
 								</>
 							)}
@@ -181,22 +182,31 @@ function Meeting({ meeting }) {
 	let endDateTime = parseISO(meeting.endDatetime);
 
 	return (
-		<li className="flex flex-col items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
-			<img
-				src={meeting.imageUrl}
-				alt=""
-				className="flex-none w-full"
-			/>
-			<div className="flex flex-col font-bold text-xl mt-4  items-center">
-				<div>
-					<p className="text-green-900">{meeting.name}</p>
+		<>
+			<li className="pb-3 sm:pb-4 bg-gray-100 hover:bg-red-200 rounded-xl p-3 duration-300">
+				<div className="flex items-center space-x-4">
+					<div className="flex-shrink-0">
+						<img
+							className="w-8 h-8 rounded-full"
+							src={logo}
+						/>
+					</div>
+					<div className="flex-1 min-w-0">
+						<p className="text-sm font-medium text-gray-900 truncate">{meeting.event}</p>
+						<p className="text-sm text-gray-500 truncate">
+							<time dateTime={meeting.startDatetime}>{format(startDateTime, "h:mm a")} - </time>
+							<time dateTime={meeting.endDatetime}>{format(endDateTime, "h:mm a")}</time>
+						</p>
+					</div>
+					<div className="inline-flex items-center text-base font-semibold text-gray-900">
+						<FontAwesomeIcon
+							icon={faBuildingLock}
+							className="animate-pulse text-xl text-red-600"
+						/>
+					</div>
 				</div>
-				<div className="flex flex-row">
-					<time dateTime={meeting.startDatetime}>{format(startDateTime, "h:mm a")}</time> -{" "}
-					<time dateTime={meeting.endDatetime}>{format(endDateTime, "h:mm a")}</time>
-				</div>
-			</div>
-		</li>
+			</li>
+		</>
 	);
 }
 
